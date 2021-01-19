7ca9fed70f5ca7e614b09e87313730cb819398c5
Simon Jacobs <simonadlerjacobs@gmail.com>
2021-01-19 17:21:59 -0500

    commit tree af606d61cc3af8bf12411ca8bb94ca82aac9b9f9

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates_length_of :summary, maximum: 250
+    validates_length_of :content, minimum: 250
+    validates :category, inclusion: { in: %w(Non-Fiction, Fiction)}
+    validate :click_bait, on: :create
+
+ CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i
+  ]
+
+  def click_bait
+    if CLICKBAIT_PATTERNS.none? {|pattern| pattern.match(self.title)}
+      self.errors.add(:title, "is not clickbait enough")
+    end
+  end
 end
